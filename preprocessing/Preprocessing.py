6d91f60896de016296932cea9f44c0dc8572cdd8
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2021-11-23 03:45:52 +0900

    Feat: create nse_augment method in Preprocessing

--- preprocessing/Preprocessing.py
+++ preprocessing/Preprocessing.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np 
 
 class Preprocessing():
-    def __init__(self, rpm):
+    def __init__(self, dataset: pd.DataFrame, rpm):
         self.rpm = rpm
         self.freq_source = rpm / 30
         self.period = 1 / self.freq_source 
+        self.dataset = dataset
 
     def fundamental_total_sample_period(self, time_sample):
         '''
@@ -16,5 +17,26 @@ class Preprocessing():
 
         return self.period / time_sample
 
-    def dft(self, data: pd.DataFrame):
-        
+    def dft(self):
+        pass 
+
+    def nsc_augment(self):
+        '''
+        Calculate NSC(Negative Sequantial Current) and add columns to pandas dataframe
+        '''
+        a = complex(-0.5, 0.866 * np.pi)
+        a2 = complex(-0.5, -0.866 * np.pi)
+        self.dataset['Max(A)'] = 0 
+        self.dataset['Max(B)'] = 0 
+        self.dataset['Max(C)'] = 0 
+        self.dataset['NSC'] = 0 
+
+        for i in range(len(self.dataset.iloc[: , 1]) - (self.period-1)):
+            self.dataset['Max(A)'][i: i + self.period] = self.dataset.iloc[i: i + self.period, 1].max()
+            self.dataset['Max(C)'][i: i + self.period] = self.dataset.iloc[i: i + self.period, 3].max()
+            self.dataset['Max(B)'][i: i + self.period] = self.dataset.iloc[i: i + self.period, 2].max()
+
+        self.dataset['NSC'] = abs(0.333 * (self.dataset.loc[:, 'Max(A)'] + a2 * self.dataset.loc[:, 'Max(B)'] +  a * self.dataset.loc[:, 'Max(C)']))
+        self.dataset.drop(['Max(A)', 'Max(B)', 'Max(C)'], axis=1, inplace=True)
+
+        return self.dataset
