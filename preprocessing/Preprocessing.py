290d4d0034e697e0184a511746b394f3ac475a6a
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2021-11-24 00:15:44 +0900

    Feat: dft method in Preprocessing
    input feature label, output DFT x_f, y_f

--- preprocessing/Preprocessing.py
+++ preprocessing/Preprocessing.py
@@ -17,8 +17,20 @@ class Preprocessing():
         self.Ns = int(self.period / self.time_sample)
         return self.Ns
 
-    def dft(self):
-        pass 
+    def dft(self, label: str):
+        len_data = len(self.dataset)
+        y_sample = self.dataset[len_data // 2: len_data // 2 + self.Ns]
+        self.freq_source
+
+        N = self.Ns 
+        df = int(self.freq_source)
+        x = np.linspace(0.0, N * df, N)
+        y = y_sample[label]
+        y_f = np.fft.fft(y)
+        y_f = 2.0/N * np.abs(y_f[: N // 2])
+        x_f = x[:N // 2]
+
+        return x_f, y_f
 
     def nsc_augment(self):
         '''
@@ -31,10 +43,10 @@ class Preprocessing():
         self.dataset['Max(C)'] = 0 
         self.dataset['NSC'] = 0 
 
-        for i in range(len(self.dataset.iloc[: , 1]) - (self.period-1)):
-            self.dataset['Max(A)'][i: i + self.period] = self.dataset.iloc[i: i + self.period, 1].max()
-            self.dataset['Max(C)'][i: i + self.period] = self.dataset.iloc[i: i + self.period, 3].max()
-            self.dataset['Max(B)'][i: i + self.period] = self.dataset.iloc[i: i + self.period, 2].max()
+        for i in range(len(self.dataset.iloc[: , 1]) - (self.Ns-1)):
+            self.dataset['Max(A)'][i: i + self.Ns] = self.dataset.iloc[i: i + self.Ns, 1].max()
+            self.dataset['Max(C)'][i: i + self.Ns] = self.dataset.iloc[i: i + self.Ns, 3].max()
+            self.dataset['Max(B)'][i: i + self.Ns] = self.dataset.iloc[i: i + self.Ns, 2].max()
 
         self.dataset['NSC'] = abs(0.333 * (self.dataset.loc[:, 'Max(A)'] + a2 * self.dataset.loc[:, 'Max(B)'] +  a * self.dataset.loc[:, 'Max(C)']))
         self.dataset.drop(['Max(A)', 'Max(B)', 'Max(C)'], axis=1, inplace=True)
